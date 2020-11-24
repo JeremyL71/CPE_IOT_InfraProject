b1d101368cf4c9a6c942c0e2ef8d3b61d9590854
LAURENT Jérémy <jlaurent@twm.ch>
2020-11-24 15:50:31 +0100

    add code

--- code microbit droite/code.py
+++ code microbit droite/code.py
@@ -11,12 +11,15 @@ clear_oled()
 print (sys.version)
 
 print("start main")
+
 conf = "TL"
 
-radio_group=1
+radio_group=51
 print("radio_group: " + str(radio_group))
 radio.config(group=radio_group)
 radio.on()
+
+msg = None
 
 initialize(pinReset=pin0)
 
@@ -31,9 +34,16 @@ while True:
     message_to_send = str(temperature())+" "+str(display.read_light_level())
     print("Message send: " + message_to_send)
     radio.send(message_to_send)
+    print("curent temp: " + str(temperature()))
+    print("curent light: " + str(display.read_light_level()))
+    sleep(1)
     
     string_temp = "temp: " + str(temperature())
     string_light = "light: " + str(display.read_light_level())
+    print("string_temp: " + string_temp)
+    print("string_light: " + string_light)
+    
+    print("conf: " + conf)
     if conf == "LT":
          add_text(0, 0, string_light)
          add_text(0, 1, string_temp)
