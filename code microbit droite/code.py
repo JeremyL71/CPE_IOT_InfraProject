782746c23b92d9649ba927020796fa1b8cf653c4
LAURENT Jérémy <jlaurent@twm.ch>
2020-11-24 10:50:23 +0100

    refresh code

--- code microbit droite/code.py
+++ code microbit droite/code.py
@@ -5,10 +5,13 @@ import radio
 from time import sleep
 import sys
 
+
+clear_oled()
+
 print (sys.version)
 
 print("start main")
-conf = "LT"
+conf = "TL"
 
 radio_group=1
 print("radio_group: " + str(radio_group))
@@ -23,18 +26,20 @@ while True:
     if msg is not None:
         print("msg: "+ str(msg))
         conf = msg
+        clear_oled()
     
     message_to_send = str(temperature())+" "+str(display.read_light_level())
+    print("Message send: " + message_to_send)
     radio.send(message_to_send)
     
     string_temp = "temp: " + str(temperature())
     string_light = "light: " + str(display.read_light_level())
     if conf == "LT":
-         add_text(0, 1, string_light)
-         add_text(0, 0, string_temp)
-    elif conf == "TL":
          add_text(0, 0, string_light)
          add_text(0, 1, string_temp)
+    elif conf == "TL":
+         add_text(0, 1, string_light)
+         add_text(0, 0, string_temp)
     print("End while")
     
         
